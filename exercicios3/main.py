2978eb70f1dfd73fd70f143b7cf3a3ad37575622
ArieleMartins <ariele.cmmelo@gmail.com>
2022-11-11 18:40:49 -0300

    Update

--- exercicios3/main.py
+++ exercicios3/main.py
@@ -61,3 +61,125 @@ else:
 
 
 '''
+
+
+'''
+
+ORDEM CRESCENTE
+
+num1 = input("Digite um numero: ")
+num2 = input('Digite mais um numero: ')
+num3 = input('Digite o ultimo numero: ')
+
+while num1.isnumeric() == False or num2.isnumeric() == False or num3.isnumeric() == False :
+    print('Por favor digite um numero')
+    num1 = input("Digite um numero: ")
+    num2 = input('Digite mais um numero: ')
+    num3 = input('Digite o ultimo numero: ')
+else:
+    n1 = int(num1)
+    n2 = int(num2)
+    n3 = int(num3)
+    if n1 > n2 and n1 > n3:
+        maior = n1
+        if n2 > n3:
+            medio = n2
+            menor = n3
+        else:
+            medio = n3
+            menor = n2
+    elif n2 > n1 and n2 > n3:
+        maior = n2
+        if n3 > n1:
+            medio = n3
+            menor = n1
+        else:
+            medio = n1
+            menor = n3
+    elif n3 > n1 and n3 > n2:
+        maior = n3
+        if n1 > n2:
+            medio = n1
+            menor = n2
+        else:
+            medio = n2
+            menor = n1
+    print('{}   {}    {}'.format(maior, medio, menor))
+
+'''
+
+'''
+
+AUMENTO DE SALARIO
+
+salario = input('Digite seu salario: ')
+
+while salario.isnumeric() == False:
+    print("Por favor digite um numero")
+    salario = input('Digite seu salario: ')
+else:
+    if int(salario) > 1250:
+        aumento = ((int(salario) * 10) / 100) + int(salario)
+        print('Seu salario aumentou para {}'.format(aumento))
+    else:
+        aumento = (((int(salario)) * 15) / 100) + int(salario)
+        print('Seu salario aumentou para {}'.format(aumento))
+'''
+
+'''
+
+from random import randint
+from time import sleep
+computador = randint(0, 5)
+print('-=-' * 18)
+print('Vou pensar em número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 18)
+jogador = int(input('Em que numero eu pensei? '))
+print('PROCESSANDO....')
+sleep(3)
+if int(jogador) == computador:
+    print('PARABÉNS! Você conseguiu me vencer!')
+else:
+    print('GANHEI! Eu pensei no número {} e não no {}'.format(computador, jogador))
+
+'''
+
+'''
+
+ÌMPAR OU PAR
+
+numero = input('Digite um numero: ')
+
+while numero.isnumeric() == False:
+    print('Por favor digite um numero')
+    numero = input('Digite um numero: ')
+else:
+    if int(numero) % 2 == 0:
+        print("Número Par")
+    else:
+        print('Número Ímpar')
+'''
+
+'''
+
+import datetime
+
+
+ano = int(input("Que ano quer analisar? Coloque 0 para analisar o ano atual \n"))
+if ano == 0:
+    ano = datetime.date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é BISSEXTO'.format(ano))
+else:
+    print('O ano {} não é BISSEXTO'.format(ano))
+
+'''
+
+r1 = float(input("Digite o primeiro segmento: "))
+r2 = float(input("Digite o segundo segmento: "))
+r3 = float(input("Digite o terceiro segmento: "))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Pode formar um triangulo')
+else:
+    print('Não pode formar um triangulo')
