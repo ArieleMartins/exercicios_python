ae704540daaaa25a3ce6ffe82483317a5e2cb567
ArieleMartins <ariele.cmmelo@gmail.com>
2022-11-11 21:28:38 -0300

    colors terminal

---
+++ terminal/main.py
@@ -0,0 +1,34 @@
+print('\033[1:31m-=-\033[m' * 10)
+print('OLÀ QUE LINDOOOOOOOO!!!!')
+print('\033[1:31m-=-\033[m' * 10)
+'''
+
+\033[style:text:backm
+
+style
+0 none
+1 bold
+4 Underline
+7 negative
+
+text
+30 branco
+31 vermelho
+32 verde
+33 amarelo
+34 azul
+35 roxo
+36 ciano
+37 cinza
+
+back
+40 branco
+41 vermelho
+42  verde
+43  amarelo
+44  azul
+45  roxo
+46  ciano
+47  cinza
+
+'''
