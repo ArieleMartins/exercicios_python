d2b76734f98c248b3e11eee440aa6293c659ba3e
ArieleMartins <ariele.cmmelo@gmail.com>
2022-11-12 19:11:09 -0300

    Update

---
+++ exercicios4/main.py
@@ -0,0 +1,199 @@
+#EMPRESTIMO
+
+'''
+
+print('\033[1:35m*=*\033[m' * 20)
+print('Emprestimo')
+print('\033[1:35m*=*\033[m' * 20)
+
+casa = input('Digite o valor da casa: ')
+salario = input('Digite seu salario: ')
+anos = input('Em quantos anos você pretente pagar: ')
+
+while casa.isnumeric() == False and salario.isnumeric() == False and anos.isnumeric() == False:
+    print('Por favor digite um numero')
+    casa = input('Digite o valor da casa: ')
+    salario = input('Digite seu salario: ')
+    anos = input('Em quantos anos você pretente pagar: ')
+else:
+    valorMes = float(casa) / (float(anos) * 12)
+    porcentoSalario = (float(valorMes) * 100) / float(salario)
+
+    if porcentoSalario > 30:
+        print('Você não poderá fazer o emprestimos, pois passa de 30% do seu salario. Porcento: {}'.format(porcentoSalario))
+    else:
+        print('PARABÉNS!!! SEU EMPRESTIMO FOI ACEITO. Porcento: {}'.format(porcentoSalario))
+
+
+
+'''
+
+'''
+
+numero = input('Digite um numero: ')
+
+while numero.isnumeric() == False:
+    print('Por favor digite um numero')
+    numero = input('Digite um numero: ')
+else:
+    
+
+'''
+
+#MAIOR E MENOR
+
+'''
+
+n1 = input("Digite um numero: ")
+n2 = input("Digite outro numero: ")
+
+while n1.isnumeric() == False and n2.isnumeric() == False:
+    print("Por favor digite somente numeros")
+    n1 = input("Digite um numero: ")
+    n2 = input("Digite outro numero: ")
+else:
+    if int(n1) > int(n2):
+        print('{} é maior que {}'.format(n1, n2))
+    elif int(n2) > int(n1):
+        print('{} é maior que {}'.format(n2, n1))
+    else:
+        print('Os valores são iguais')
+
+'''
+
+#ALISTAMENTO
+
+'''
+
+import datetime
+
+anoNasc = input('Digite o seu ano de nascimento: ')
+
+while anoNasc.isnumeric() == False:
+    print('Por favor digite somente numeros')
+    anoNasc = input('Digite o seu ano de nascimento: ')
+else:
+    anoAtual = datetime.date.today().year
+    idade = anoAtual - int(anoNasc)
+    if idade == 18:
+        print("Você deve se alistar")
+    elif idade > 18:
+        print('Já passo da hora de você se alistar')
+        passo = idade - 18
+        print('Passou {} anos do pedido para se alistar'.format(passo))
+    else:
+        falta = 18 - idade
+        print('Você ainda vai se alistar')
+        print('Falta {} anos para você se alistar'.format(falta))
+
+'''
+
+#MEDIA
+
+'''
+
+nota1 = input("Digite a primeira nota: ")
+nota2 = input("Digite a segunda nota: ")
+
+while nota1.isnumeric() == False and nota2.isnumeric() == False:
+    print("Por favor digite somente numeros")
+    nota1 = input("Digite a primeira nota: ")
+    nota2 = input("Digite a segunda nota: ")
+else:
+    media = (float(nota1) + float(nota2)) / 2
+    print("\033[0:35m-=-\033[m" * 10)
+    if media  < 5:
+
+        print('\033[0:31mREPROVADO\033[m')
+    elif media > 5 and media < 6.9:
+        print("\033[0:34mRECUPERAÇÃO\033[m")
+    else:
+        print("\033[0:32mAPROVADO\033[m")
+    print("\033[0:35m-=-\033[m" * 10)
+
+'''
+
+#CATEGORIA NATACAO
+
+'''
+
+import datetime
+
+print("\033[0:35m-=-\033[m" * 12)
+print("CONFEDERAÇÃO NACIONAL DE NATAÇÃO")
+print("\033[0:35m-=-\033[m" * 12)
+
+anoNasc = input('Digite seu ano de nascimento: ')
+
+while anoNasc.isnumeric() == False:
+    print("Por favor digite somente numero")
+    anoNasc = input('Digite seu ano de nascimento: ')
+else:
+    idade = datetime.date.today().year - int(anoNasc)
+
+    if idade <= 9:
+        print("CATEGORIA: MIRIM")
+    elif idade > 9 and idade <= 14:
+        print("CATEGORIA: INFANTIL")
+    elif idade > 14 and idade <= 19:
+        print("CATEGORIA: JUNIOR")
+    elif idade == 20:
+        print("CATEGORIA: SÊNIOR")
+    else:
+        print("CATEGORIA: MASTER")
+
+'''
+
+#TRIANGULO
+
+'''
+
+r1 = float(input("Digite o primeiro segmento: "))
+r2 = float(input("Digite o segundo segmento: "))
+r3 = float(input("Digite o terceiro segmento: "))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Pode formar um triangulo')
+    if r1 == r2 and r2 == r3 and r1 == r3:
+        print('Equilátero')
+    elif r1 == r2 or r2 == r3 or r1 == r3:
+        print('Isósceles')
+    else:
+        print('Escaleno')
+else:
+    print('Não pode formar um triangulo')
+
+'''
+
+#IMC
+
+'''
+
+print("\033[0:35m-=-\033[m" * 12)
+print("CALCULANDO IMC")
+print("\033[0:35m-=-\033[m" * 12)
+
+altura = input("Digite sua altura: ")
+peso = input("Digite seu peso: ")
+
+while altura.isnumeric() == False and peso.isnumeric() == False:
+    print('Por favor digite somente numeros')
+    altura = input("Digite sua altura: ")
+    peso = input("Digite seu peso: ")
+else:
+    imc = float(peso) / (float(altura.replace(',', '.')) * float(altura.replace(',', '.')))
+    if imc < 18.5:
+        print("Você esta abaixo do peso. IMC: {:.2f}".format(imc))
+    elif imc >= 18.5 and imc <= 25:
+        print("Você esta no peso ideal. IMC: {:.2f}".format(imc))
+    elif imc > 25 and imc <= 30:
+        print("Você esta com sobrepeso. IMC: {:.2f}".format(imc))
+    elif imc > 30 and imc <= 40:
+        print("Você esta com obesidade. IMC: {:.2f}".format(imc))
+    else:
+        print("Você esta com obesidade mórbida. IMC: {:.2f}".format(imc))
+
+'''
+
+
+
