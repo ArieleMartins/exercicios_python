12c1917b95e00b7a3ddf9a4d5ccc3df1e5d79e81
ArieleMartins <ariele.cmmelo@gmail.com>
2022-11-15 16:03:12 -0300

    Update Studying Loop For

---
+++ exercicios5/main.py
@@ -0,0 +1,79 @@
+#CONTAGEM
+
+'''
+
+import time
+
+print("\033[0:31m-*-\033[m" * 10)
+print("\033[0:32m{:^30}\033[m".format("FELIZ ANO NOVO"))
+print("\033[0:31m-*-\033[m" * 10)
+
+print('E QUE COMECE A CONTAGEM')
+
+for interval in range(10, 0, -1):
+    print("{} !!!".format(interval))
+    time.sleep(1)
+print("\033[0:31m-*-\033[m" * 10)
+print("\033[0:32m{:^30}\033[m".format("FELIZ ANO NOVO !!!!!"))
+print("\033[0:31m-*-\033[m" * 10)
+
+'''
+
+#PARES ENTRE 1 E 50
+
+'''
+
+for interval in range(1, 50):
+    if interval % 2 == 0:
+        print(interval)
+
+'''
+
+#NUMERO IMPARES MULTIPLOS DE TRES
+
+'''
+
+for interval in range(1, 500):
+    if interval % 3 == 0:
+        print(interval)
+
+'''
+
+#TABUADA
+
+'''
+
+numero = input('Digite o numero que deseja ver a tabuada: ')
+
+while numero.isnumeric() == False:
+    print('Por favor digite somente numeros')
+    numero = input('Digite o numero que deseja ver a tabuada: ')
+else:
+    for interval in range(0, 11):
+        tabuada = int(numero) * interval
+        print('{} X {} = {}'.format(interval, numero, tabuada))
+
+'''
+
+
+#SOMA DE NUMEROS PARES
+
+'''
+
+soma = 0
+
+for interval in range(0, 6):
+    numero = input('Digite um numero: ')
+    if int(numero) % 2 == 0:
+        soma = soma + int(numero)
+
+print("A soma dos numero pares é {}".format(soma))
+
+'''
+
+
+
+
+
+
+
