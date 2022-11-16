876c010ed40a7820d6670dc6dd4c4f5b5bfdedc9
ArieleMartins <ariele.cmmelo@gmail.com>
2022-11-16 18:58:28 -0300

    Update Exercicios com While

---
+++ exercicios6/main.py
@@ -0,0 +1,13 @@
+# PRATICANDO WHILE
+
+'''
+
+sexo = input("Digite seu sexo: [M/F] ")
+while sexo.upper() != 'M' and sexo.upper() != 'F':
+    print('Por favor digite M ou F')
+    sexo = input("Digite seu sexo: [M/F] ")
+else:
+    print('O seu sexo é {}'.format(sexo))
+
+'''
+
