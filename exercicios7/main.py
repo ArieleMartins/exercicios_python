b701aba4a0f6adb5256b4ddb2817b7becf8f6185
ArieleMartins <ariele.cmmelo@gmail.com>
2022-11-18 19:30:57 -0300

    Update impar ou par

--- exercicios7/main.py
+++ exercicios7/main.py
@@ -18,7 +18,7 @@ print(f'Você digite o total de {count} números e a soma entre ele é {sum}')
 
 '''
 
-# tabuada
+# TABUADA
 
 '''
 
@@ -41,4 +41,38 @@ else:
 
 '''
 
+#JOGO IMPAR OU PAR
 
+'''
+
+import random
+
+print('=-' * 10)
+print('ÍMPAR OU PAR')
+print('=-' * 10)
+
+number = 1
+while int(number) != '0':
+    bot = random.randint(0, 10)
+    number = input('Diga o valor:')
+    while number.isnumeric() == False:
+        print('Por favor digite somente números')
+        number = input('Diga o valor: ')
+    else:
+        option = input('Par ou Ímpar: [P/I] ')
+        while option.upper() != "P" and option.upper() != "I":
+            print("Por favor digite P ou I")
+            option = input('Par ou Ímpar: [P/I] ')
+        else:
+            sum = bot + int(number)
+            if sum % 2 == 0:
+                if option.upper() == 'P':
+                    print('Você ganhou')
+                else:
+                    print('A máquina ganhou')
+            else:
+                break
+
+print('Você perdeu')
+
+'''
