43fb46a80a31deb9be3a93b5e2aa696326249731
ArieleMartins <ariele.cmmelo@gmail.com>
2022-12-07 19:38:48 -0300

    :new: Tabela do brasileirão

--- exercicios8/main.py
+++ exercicios8/main.py
@@ -1,5 +1,8 @@
 # aprendendo tuplas
 
+
+'''
+
 number = input('Digite um número de 0 a 20: ')
 
 while number.isnumeric() == False or int(number) < 0 or int(number) > 20:
@@ -7,4 +10,21 @@ while number.isnumeric() == False or int(number) < 0 or int(number) > 20:
 else:
     numbers = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
     print(f'Você digitou o número {numbers[int(number)]}')
+
+'''
+
+# TABELA DO BRASILEIRÃO
+
+'''
+
+classificacao = ('Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Flamengo', 'Athletico-PR', 'Atlético-MG', 'Fortaleza', 'São Paulo', 'Amérca-MG', 'Botafogo', 'Santos', 'Goiás', 'Bragantino', 'Coritiba', 'Cuiabá', 'Ceará SC', 'Atlético-GO', 'Avaí', 'Juventude')
+
+print(f'Os cinco primeiros colocados: \n{classificacao[0:6]}')
+print(f'Os ultimo 4 colocados: \n{classificacao[16:21]}')
+print(f'{sorted(classificacao)}')
+print(f'O Atlético-MG está na posicao {classificacao.index("Atlético-MG")}')
+
+'''
+
+
 
